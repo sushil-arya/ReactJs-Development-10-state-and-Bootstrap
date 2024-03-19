3f4a7795f9c755163bfbbe013b6f781d00eef4a8
sushil-arya <sushilarya888@gmail.com>
2024-03-20 00:59:18 +0530

    simple-html-page-using-bootstrap in index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import './Style.css';
+
+const root = ReactDOM.createRoot(document.querySelector(".container-fluid"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <>
+    <header>Header</header>
+    <main>
+      <div className="container h-100 a_cont">
+        <div className="row h-100">
+          <aside className="col-2 s_leftaside tbdr">LeftAside</aside>
+          <section className="col-8 tbdr">Section</section>
+          <aside className="col-2 s_rightaside tbdr">RightAside</aside>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        </div>
+        
+
+      </div>
+    </main>
+    <footer>Footer</footer>
+  </>
+);
